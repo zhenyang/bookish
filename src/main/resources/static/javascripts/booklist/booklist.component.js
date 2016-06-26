9d4c35d72114aa0aa4b4e89b494a9a4942027575
Zhen Yang <zyang@thoughtworks.com>
2016-06-26 12:57:03 +0800

    add pagination

--- src/main/resources/static/javascripts/booklist/booklist.component.js
+++ src/main/resources/static/javascripts/booklist/booklist.component.js
@@ -1,11 +1,24 @@
-angular.module('bookList')
-    .component('bookList', {
-        templateUrl: 'javascripts/booklist/booklist.template.html',
-        controller: ['Book',
-            function BookListController(Book) {
-                var self = this;
-                Book.query(function (response) {
+angular.module('bookList').component('bookList', {
+    templateUrl: 'javascripts/booklist/booklist.template.html',
+    controller: ['$routeParams', 'Book',
+        function BookListController($routeParams, Book) {
+            var self = this;
+            self.pageIndex = $routeParams.page || 1;
+            self.pageSize = 12;
+            Book.query(
+                {page: self.pageIndex, size: self.pageSize},
+                function (response) {
+                    self.pages = range(response.totalPages);
                     self.books = response.content;
                 });
-            }]
-    });
+
+            function range(n) {
+                var res = [];
+                for (var i = 0; i < n; i++) {
+                    res.push(i);
+                }
+                return res;
+            }
+
+        }]
+});
